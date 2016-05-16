353288eec643bffb51aca3ea7d0001cff4d3c867
r-miyamoto <r-miyamoto@dip-net.co.jp>
2016-05-16 19:19:08 +0900

    add EntityDataManager Functions

--- TABIsta/EntityDataManager.swift
+++ TABIsta/EntityDataManager.swift
@@ -9,8 +9,35 @@
 import RealmSwift
 
 class EntityDataManager: NSObject {
-    static func getPickupMedia() -> [MediaEntity] {
-        let rtn = [MediaEntity(), MediaEntity()]
-        return rtn;
+    // MARK: - PickupMedia
+    static func getAllPickupMedia() -> Results<MediaEntity> {
+        let realm = try! Realm()
+        let items = realm.objects(MediaEntity)
+        return items;
     }
+    
+    static func addPickupMedia(item:MediaEntity) {
+        let realm = try! Realm()
+        try! realm.write {
+            realm.add(item)
+        }
+    }
+    
+    static func deletePickupMedia(item:MediaEntity) {
+        let realm = try! Realm()
+        let deleteItem = self.getAllPickupMedia().filter("id == " + String(item.id))
+        try! realm.write {
+            realm.delete(deleteItem)
+        }
+    }
+    
+    static func deleteAllPickupMedia() {
+        let realm = try! Realm()
+        try! realm.write {
+            realm.deleteAll()
+        }
+    }
+    
+    // MARK: - Login
+    
 }
